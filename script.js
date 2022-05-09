43ecf1bf46254f0110720d2b6e690dc153388df5
Akiva Majowka <akivamaj@gmail.com>
2022-05-09 22:34:07 +0300

    interaction with DOM elements ready

--- script.js
+++ script.js
@@ -10,85 +10,85 @@ function computerPlay() {
   return randomNumber == 0 ? "scissors" : randomNumber == 1 ? "paper" : "rock";
 }
 
-// getting players round play
-function UserSelection() {
-  let rock = document.querySelector(".rock")
-  rock.addEventListener('click', SetPlayerSelection)
+function roundWinner(playerPlay, systemPlay) {
+  if ((playerPlay === 'rock' && systemPlay === 'scissors') ||
+     (playerPlay === 'scissors' && systemPlay === 'paper') ||
+     (playerPlay === 'paper' && systemPlay === 'rock')) {
+      return 'player'      
+     } else if (playerPlay === systemPlay){
+       return 'tie'
+     } else {
+       return 'system'
+     }
+}
+
+function playRound(e) {
+  let playerPlay =  e.target.innerText
+  let systemPlay = computerPlay()
+  let displayUserSelection = document.querySelector(".user-selection-display")
+  let displaySystemSelection = document.querySelector(".system-selection-display")
+  let displayRoundWinner = document.querySelector('.round-winner')
+  let displayPlayerPoints = document.querySelector(".user-points")
+  let displaySystemPoints = document.querySelector(".system-points")
   
-  let paper = document.querySelector(".paper")
-  paper.addEventListener('click', SetPlayerSelection)
+  let winner = roundWinner(playerPlay, systemPlay)
 
-  let scissors = document.querySelector(".scissors")
-  scissors.addEventListener('click', SetPlayerSelection)
+  //displays players and systems play
+  displayUserSelection.innerText = playerPlay
+  displaySystemSelection.innerText = systemPlay
+
+  if (winner === 'player') {
+    displayRoundWinner.innerText = 'You Won!'
+    playerPoints++
+    displayPlayerPoints.innerText = playerPoints
+  } else if (winner === 'system') {
+    displayRoundWinner.innerText = 'Computer Won!'
+    systemPoints++
+    displaySystemPoints.innerText = playerPoints
+  } else {
+    displayRoundWinner.innerText = 'Its a tie!!'
+  }
+
+  let gameWinner;
+  if (playerPoints === 5) {
+    alert('you won the game!')
+    displayUserSelection.innerText = ''
+    displaySystemSelection.innerText = ''
+    displayRoundWinner.innerText = ''
+    playerPoints = 0
+    systemPoints = 0
+    displayPlayerPoints.innerText = 0
+    displaySystemPoints.innerText = 0
+  } else if (systemPoints === 5) {
+    alert('computer won the game!')
+    displayUserSelection.innerText = ''
+    displaySystemSelection.innerText = ''
+    displayRoundWinner.innerText = ''
+    playerPoints = 0
+    systemPoints = 0
+    displayPlayerPoints.innerText = 0
+    displaySystemPoints.innerText = 0
+  }
 }
 
-function SetPlayerSelection(e) {
-   playerSelection =  e.target.innerText
-}  
+let playerPoints = 0;
+let systemPoints = 0;
 
-//Checks who is the winnner between user and computer
-function playRound(playerSelection, computerSelection) {
-  if (
-    (playerSelection == "scissors" && computerSelection == "paper") ||
-    (playerSelection == "rock" && computerSelection == "scissors") ||
-    (playerSelection == "paper" && computerSelection == "rock")
-  ) {
-    return "user";
-  }
-  if (playerSelection == computerSelection) {
-    return "tie";
-  }
-  return "computer";
-}
-
-function logic() {
-  let userPoints = 0;
-  let computerPoints = 0;
-
-  window.playerSelection = null;
-  let computerSelection = computerPlay();
-
-  for (let i = 1; i < 5; i++) {
-    let winner = playRound(playerSelection, computerSelection);
-
-    if (winner == "user") {
-      console.log('you won this round!')
-      userPoints++;
-    } else if (winner == "computer") {
-      console.log('you lost this round!')
-      computerPoints++;
-    } else if (winner == 'tie'){
-      console.log("We have a tie!");
-      continue;
-    } else {
-      console.log("Please choose Rock, Paper or Scissors");
-      i--
-    }
-    console.log(`computer: ${computerPoints}, you: ${userPoints}`);
-  }
-
-    let gameWinner;
-
-    if (userPoints > computerPoints) {
-      gameWinner = "You won, congradulations!";
-    } else if (computerPoints > userPoints) {
-      gameWinner = "The Computer won, best of luck next time";
-    } else {
-      gameWinner = "The game finished as a tie!";
-    }
-    console.log(gameWinner);
+let rockButton = document.querySelector(".rock")
+rockButton.addEventListener('click', playRound)
   
-}
+let paperButton = document.querySelector(".paper")
+paperButton.addEventListener('click', playRound)
 
-game()
+let scissorsButton = document.querySelector(".scissors")
+scissorsButton.addEventListener('click', playRound)
 
+// jogador aperta um dos 3 botoes que roda uma funcao que faz os seguintes passos:
 
-// botao iniciar mostra o jogo
-// elementos DOM sao aplicados a variaveis, 
-
-// rodada comeca com a escolha de jogada do jogador
-// escolhe um valor "pedra papel ou tesoura" de forma aleatoria pra o jogo,
-// verifica o vencedor //  vencedor eh salvo em sua variavel adicionando um numero
+// playRound()
+// usa o valor do botao apertado para escolher a jogada do jogador
+// escolhe uma jogada de forma aleatoria pra o jogo
+// verifica o vencedor, vencedor eh salvo em sua variavel adicionando um numero
 // mostra na tela a escolha do jogador contra a do jogo, e abaixo quem vence a rodada
 
 // um teste eh feito para verificar se um dos jogadores tem cinco pontos 
@@ -96,4 +96,3 @@ game()
 // se nao, o jogo prossegue pro proximo passo
 
 // atualiza os pontos apresentados 
-// devolve a tela de escolha de jogada
